43975f21555f2be09dfdb0eefe3c132f47b056a3
TreleaniFederico <treleani.federico@gmail.com>
2022-09-20 16:14:31 +0200

    add js axios and for cicle to print random mails

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log('js ok');
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        mails: [],
+        mailNumbers: 10,
+    },
+    mounted() {
+
+        for (let i = 0; i < 10; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+
+                .then((response) => {
+                    const mail = response.data.response
+                    this.mails.push(mail);
+                    // this.mails.push(response.data.response);
+                });
+        }
+    }
+})
+
+
+
