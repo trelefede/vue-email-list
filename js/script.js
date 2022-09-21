3b61723968e8b60d61624420f1bfb20a25105436
TreleaniFederico <treleani.federico@gmail.com>
2022-09-21 15:35:39 +0200

    add security improvements

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
 console.log('js ok');
 
+const NUM_EMAILS = 10;
+
 const app = new Vue({
     el: '#app',
     data: {
+        charged: true,
         mails: [],
     },
     mounted() {
 
-        const array = [];
-        for (let i = 0; i < 10; i++) {
+        let countDown = 0;
+        for (let i = 0; i < NUM_EMAILS; i++) {
             axios
                 .get('https://flynn.boolean.careers/exercises/api/random/mail')
-
                 .then((response) => {
-                    array.push(response.data.response);
+                    const { data, status } = response;
+                    countDown++;
+                    if (status === 200) {
+                        this.mails.push(data.response);
+                    }
+                    if (countDown === NUM_EMAILS) {
+                        this.charged = false;
+                    }
+
                 });
         }
-        this.mails = array;
     }
 })
 
